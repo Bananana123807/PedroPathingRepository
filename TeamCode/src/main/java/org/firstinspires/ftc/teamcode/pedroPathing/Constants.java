e96922becbf7d841ff0d0709463502f93b0e1375
Aarush <aarush.d.shah@outlook.com>
2025-09-21 16:14:19 -0700

    new

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -36,7 +36,8 @@ public class Constants {
                     )
             )
             .forwardPodY(5)
-            .strafePodX(4);
+            .strafePodX(4)
+            .strafeEncoderDirection(Encoder.REVERSE);
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
                 .pathConstraints(pathConstraints)

9394c38360435736cbe3f20cdf4ad2b70f07ef2c
Akshansh Chaurishi <480697@bsd48.org>
2025-09-27 07:24:58 -0700

    fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/OCEAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/OCEAuto.java
@@ -7,7 +7,7 @@ import com.pedropathing.paths.Path;
 import com.pedropathing.paths.PathChain;
 import com.pedropathing.util.Timer;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import  com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 @Autonomous(name = "OCEAuto", group = "Over-caffeinated")
 public class OCEAuto extends OpMode {

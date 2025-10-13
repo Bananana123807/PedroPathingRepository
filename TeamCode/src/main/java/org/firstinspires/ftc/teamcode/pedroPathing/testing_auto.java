73f856b85937bfc40c9a88edadb8005b37a9d99a
Aarush <aarush.d.shah@outlook.com>
2025-10-12 17:10:57 -0700

    tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/testing_auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/testing_auto.java
@@ -1,4 +1,7 @@
-package org.firstinspires.ftc.teamcode.pedroPathing; // make sure this aligns with class location
+package org.firstinspires.ftc.teamcode.pedroPathing;
+
+import static java.lang.Thread.currentThread;
+import static java.lang.Thread.sleep;
 
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.BezierLine;
@@ -8,86 +11,87 @@ import com.pedropathing.paths.PathChain;
 import com.pedropathing.util.Timer;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
-@Autonomous(name = "testing_auto_OCE", group = "Over-caffeinated")
+@Autonomous(name = "ScorePreload", group = "Over-caffeinated")
 public class testing_auto extends OpMode {
-
+    private double shooterPower = -1;
+    private double gatePower = -1;
     private Follower follower;
-    private Timer pathTimer, actionTimer, opmodeTimer;
-
+    private Timer pathTimer;
+    private DcMotor shooterMotor = null;
+    private CRServo gate = null;
     private int pathState;
-
-    private final Pose startPose = new Pose(0, 0, Math.toRadians(90)); // Start Pose of our robot.
-    private final Pose scorePose = new Pose(0   , 24, Math.toRadians(90));
-    private final Pose secondPose = new Pose(24, 24, Math.toRadians(90));
-    // Scoring Pose of our robot. It is facing the goal at a 135 degree angle.
+    private double waitTime = 1000;
+    private boolean waiting = false;
+    private final Pose startPose = new Pose(0, 0);
+    private final Pose scorePose = new Pose(-60, 0);
     private Path scorePreload;
-    private PathChain secondRun;
-
     public void buildPaths() {
-        /* This is our scorePreload path. We are using a BezierLine, which is a straight line. */
         scorePreload = new Path(new BezierLine(startPose, scorePose));
-        scorePreload.setConstantHeadingInterpolation(scorePose.getHeading());
-
-        secondRun = follower.pathBuilder()
-                .addPath(new BezierLine(scorePose, secondPose))
-                .setConstantHeadingInterpolation(scorePose.getHeading())
-                .build();
+        scorePreload.setConstantHeadingInterpolation(Math.toRadians(0));
     }
 
     public void setPathState(int pState) {
         pathState = pState;
         pathTimer.resetTimer();
     }
-    public void autonomousPathUpdate() {
+    public void autonomousPathUpdate(){
         switch (pathState) {
             case 0:
                 follower.followPath(scorePreload);
                 setPathState(1);
+                pathTimer.resetTimer();
+                telemetry.addLine("Moving Back");
                 break;
             case 1:
-                follower.followPath(secondRun);
-                setPathState(2);
+                shooterMotor.setPower(shooterPower);
+                gate.setPower(gatePower);
+//                if (pathTimer.getElapsedTimeSeconds() >= 1){
+//                    telemetry.addLine("Shooting");
+//
+//                } else {
+//                    telemetry.addLine("Waiting");
+//                    setPathState(1);
+//                }
+                break;
         }
     }
-        @Override
-        public void loop() {
 
-            // These loop the movements of the robot, these must be called continuously in order to work
-            follower.update();
-            autonomousPathUpdate();
+    @Override
+    public void loop() {
+        follower.update();
+        autonomousPathUpdate();
 
-            // Feedback to Driver Hub for debugging
-            telemetry.addData("path state", pathState);
-            telemetry.addData("x", follower.getPose().getX());
-            telemetry.addData("y", follower.getPose().getY());
-            telemetry.addData("heading", follower.getPose().getHeading());
-            telemetry.update();
-        }
-
-        /** This method is called once at the init of the OpMode. **/
-        @Override
-        public void init(){
-            pathTimer = new Timer();
-            opmodeTimer = new Timer();
-            opmodeTimer.resetTimer();
+        telemetry.addData("path state", pathState);
+        telemetry.addData("x", follower.getPose().getX());
+        telemetry.addData("y", follower.getPose().getY());
+        telemetry.addData("heading", follower.getPose().getHeading());
+        telemetry.addData("Shooter Power", shooterMotor.getPower());
+        telemetry.update();
 
 
-            follower = Constants.createFollower(hardwareMap);
-            follower.setStartingPose(startPose);
-            buildPaths();
+    }
 
-        }
 
-        /** This method is called continuously after Init while waiting for "play". **/
-        @Override
-        public void init_loop() {}
+    /** This method is called once at the init of the OpMode. **/
+    @Override
+    public void init(){
 
-        /** This method is called once at the start of the OpMode.
-         * It runs all the setup actions, including building paths and starting the path system **/
-        @Override
-        public void start() {
-            opmodeTimer.resetTimer();
-            setPathState(0);
-        }
+        shooterMotor = hardwareMap.get(DcMotor.class, "shooterMotor");
+        gate = hardwareMap.get(CRServo.class, "gate");
+
+        pathTimer = new Timer();
+        pathTimer.resetTimer();
+
+        setPathState(0);
+
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(startPose);
+        buildPaths();
+
+
+        telemetry.update();
+    }
 }
